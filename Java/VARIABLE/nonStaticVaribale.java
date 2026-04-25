06c33b7b67b218dc2e91124d29416f81b1a1cf76
ADITYA BASANTIA <adityabasantia729@gmail.com>
2026-04-25 22:33:52 +0530

    abstractClass

---
+++ Java/VARIABLE/nonStaticVaribale.java
@@ -0,0 +1,9 @@
+package VARIABLE;
+
+public class nonStaticVaribale {
+     int b = 20; 
+     System.out.println(b)
+    public static void main(String[] args) {
+        System.out.println("called");
+    }
+}
---
+++ Java/adi.java
@@ -0,0 +1,8 @@
+public class adi {
+
+    public static void main(String[] args) {
+        String a="aditya";
+        a="d";
+        System.out.println(a);
+    }
+}
